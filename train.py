46c434a03e88ccb1d9e44b0fd23cf8bfabdd7826
Hamad Ullah <hullah@iastate.edu>
2021-05-10 00:25:04 -0700

    Better model

--- train.py
+++ train.py
@@ -4,8 +4,9 @@ from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
 from keras.models import Sequential
 from keras.datasets import mnist, cifar10, cifar100
 import random
-
-
+import os.path
+from os import path
+import tensorflow as tf
 class model:
     """Training model cifar10, cifar100, and minist  and saves them into folder"""
 
@@ -110,32 +111,45 @@ class model:
 
             (X_train, Y_train),(X_test, Y_test) = mnist.load_data()
             X_train = X_train.reshape(X_train.shape[0],28,28,1)
-            X_test = X_test.reshape(X_test.shape[0],28,28,1) 
+            X_test = X_test.reshape(X_test.shape[0],28,28,1)
+            M_X_train = X_train
+            M_Y_train = Y_train
             #model 1
-            modelA = Sequential()
-            modelA.add(Conv2D(32,(3,3), activation='relu', input_shape=(28,28,1)))
-            modelA.add(MaxPooling2D(2,2))
-            modelA.add(Flatten())
-            modelA.add(Dense(100,activation='relu'))
-            modelA.add(Dense(10, activation='softmax'))
-            #model.compile(loss='binary_crossentropy', optimizer = 'sgd',metrics=['accuracy'])
-            #model.fit(X_train,Y_train, epochs=100, batch_size=32,verbose=0)
-            #model.save('saved_models/mnist')
-            modelA.compile(loss='sparse_categorical_crossentropy', optimizer = 'sgd', metrics=['accuracy'])
-            modelA.fit(X_train, Y_train , epochs=200, batch_size=16, verbose=1)
-            modelA.save('saved_models/MNIST_1')
+            if not(path.exists("saved_models/MNIST_1.h5")):
+                modelA = Sequential()
+                modelA.add(Conv2D(32,(3,3), activation='relu', input_shape=(28,28,1)))
+                modelA.add(MaxPooling2D(2,2))
+                modelA.add(Flatten())
+                modelA.add(Dense(100,activation='relu'))
+                modelA.add(Dense(10, activation='softmax'))
+                #model.compile(loss='binary_crossentropy', optimizer = 'sgd',metrics=['accuracy'])
+                #model.fit(X_train,Y_train, epochs=100, batch_size=32,verbose=0)
+                #model.save('saved_models/mnist')
+                modelA.compile(loss='sparse_categorical_crossentropy', optimizer = 'sgd', metrics=['accuracy'])
+                modelA.fit(X_train, Y_train , epochs=20, batch_size=32, verbose=1)
+                modelA.save('saved_models/MNIST_1.h5')
+            else:
+                print("else option")
+                new_model = tf.keras.models.load_model('saved_models/MNIST_1.h5')
+                
+
 
             
             #model 2
-            modelB = Sequential()
-            modelB.add(Conv2D(32,(3,3), activation='relu', input_shape=(28,28,1)))
-            modelB.add(MaxPooling2D(2,2))
-            modelB.add(Flatten())
-            modelB.add(Dense(100,activation='relu'))
-            modelB.add(Dense(10, activation='softmax'))
-            modelB.compile(loss='binary_crossentropy', optimizer = 'sgd', metrics=['accuracy'])
-            modelB.fit(X_train, Y_train , epochs=200, batch_size=16, verbose=1)
-            modelB.save('saved_models/MNIST_2')
+            if not(path.exists("saved_models/MNIST_2.h5")):
+                M_Y_train = self.binary_data(M_Y_train)
+                modelB = Sequential()
+                modelB.add(Conv2D(32,(3,3), activation='relu', input_shape=(28,28,1)))
+                modelB.add(MaxPooling2D(2,2))
+                modelB.add(Flatten())
+                modelB.add(Dense(100,activation='relu'))
+                modelB.add(Dense(10, activation='softmax'))
+                modelB.compile(loss='binary_crossentropy', optimizer = 'sgd', metrics=['accuracy'])
+                modelB.fit(M_X_train, M_Y_train , epochs=20, batch_size=32, verbose=1)
+                modelB.save('saved_models/MNIST_2.h5')
+            else:
+                print("else option")
+                new_model = tf.keras.models.load_model('saved_models/MNIST_2.h5')
 
 
 
@@ -147,6 +161,16 @@ class model:
             model = self.CNN(i)
         '''
         model = self.CNN('MNIST')
+
+    def binary_data(self, Y_train):
+        array = []
+        for i in range(len(Y_train)):
+            sam = np.zeros(10)
+            sam[Y_train[i]]+=1
+            #print(sam, Y_sample[i])
+            array.append(sam)
+        array = np.array(array)
+        return array
 
 
 if __name__=="__main__":
