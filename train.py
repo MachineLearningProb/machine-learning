b1b21df45e2950495a823eb53b9b0f62b231c6e2
Hamad Ullah <hullah@iastate.edu>
2021-05-10 00:55:19 -0700

    Added flag for training if flag is set to True

--- train.py
+++ train.py
@@ -9,7 +9,8 @@ from os import path
 import tensorflow as tf
 class model:
     """Training model cifar10, cifar100, and minist  and saves them into folder"""
-
+    def __init__(self):
+        self.flag = False
 
     def CNN(self, model):
         if model=="CIFAR10":
@@ -18,42 +19,48 @@ class model:
             X_test = X_test.astype('float32')
             img_width, img_height, img_num_channels = 32, 32, 3
             input_shape = (img_width, img_height, img_num_channels)
+            M_X_train = X_train
+            M_Y_train = Y_train
+
 
 
             X_train = X_train/255
             
             #model 1
-            modelA = Sequential()
-            modelA.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-            modelA.add(MaxPooling2D(pool_size=(2, 2)))
-            modelA.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-            modelA.add(MaxPooling2D(pool_size=(2, 2)))
-            modelA.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-            modelA.add(MaxPooling2D(pool_size=(2, 2)))
-            modelA.add(Flatten())
-            modelA.add(Dense(256, activation='relu'))
-            modelA.add(Dense(128, activation='relu'))
-            modelA.add(Dense(10, activation='softmax'))
-            modelA.compile(loss='sparse_categorical_crossentropy', optimizer = 'sgd', metrics=['accuracy'])
-            modelA.fit(X_train, Y_train , epochs=200, batch_size=16, verbose=1)
-            modelA.save('saved_models/cifar10_1')
+            if not(path.exists('saved_models/cifar10_1')) or self.flag:
+                modelA = Sequential()
+                modelA.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+                modelA.add(MaxPooling2D(pool_size=(2, 2)))
+                modelA.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+                modelA.add(MaxPooling2D(pool_size=(2, 2)))
+                modelA.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+                modelA.add(MaxPooling2D(pool_size=(2, 2)))
+                modelA.add(Flatten())
+                modelA.add(Dense(256, activation='relu'))
+                modelA.add(Dense(128, activation='relu'))
+                modelA.add(Dense(10, activation='softmax'))
+                modelA.compile(loss='sparse_categorical_crossentropy', optimizer = 'sgd', metrics=['accuracy'])
+                modelA.fit(X_train, Y_train , epochs=200, batch_size=8, verbose=1)
+                modelA.save('saved_models/cifar10_1')
 
 
             #model 2
-            modelB = Sequential()
-            modelB.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-            modelB.add(MaxPooling2D(pool_size=(2, 2)))
-            modelB.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-            modelB.add(MaxPooling2D(pool_size=(2, 2)))
-            modelB.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-            modelB.add(MaxPooling2D(pool_size=(2, 2)))
-            modelB.add(Flatten())
-            modelB.add(Dense(256, activation='relu'))
-            modelB.add(Dense(128, activation='relu'))
-            modelB.add(Dense(10, activation='softmax'))
-            modelB.compile(loss='binary_crossentropy', optimizer = 'sgd', metrics=['accuracy'])
-            modelB.fit(X_train, Y_train , epochs=200, batch_size=16, verbose=1)
-            modelB.save('saved_models/cifar10_2')
+            if not(path.exists('saved_models/cifar10_2.h5')) or self.flag:
+                M_Y_train = self.binary_data(M_Y_train)
+                modelB = Sequential()
+                modelB.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+                modelB.add(MaxPooling2D(pool_size=(2, 2)))
+                modelB.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+                modelB.add(MaxPooling2D(pool_size=(2, 2)))
+                modelB.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+                modelB.add(MaxPooling2D(pool_size=(2, 2)))
+                modelB.add(Flatten())
+                modelB.add(Dense(256, activation='relu'))
+                modelB.add(Dense(128, activation='relu'))
+                modelB.add(Dense(10, activation='softmax'))
+                modelB.compile(loss='binary_crossentropy', optimizer = 'sgd', metrics=['accuracy'])
+                modelB.fit(M_X_train, M_Y_train , epochs=200, batch_size=8, verbose=1)
+                modelB.save('saved_models/cifar10_2')
 
         elif model=="CIFAR100":
             (X_train, Y_train),(X_test, Y_test) = cifar10.load_data()
@@ -61,45 +68,51 @@ class model:
             X_test = X_test.astype('float32')
             img_width, img_height, img_num_channels = 32, 32, 3
             input_shape = (img_width, img_height, img_num_channels)
-
+            M_X_train = X_train
+            M_Y_train = Y_train
+            
+    
 
             X_train = X_train/255
 
             #model 1
-            modelA = Sequential()
-            modelA.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-            modelA.add(MaxPooling2D(pool_size=(2, 2)))
-            modelA.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-            modelA.add(MaxPooling2D(pool_size=(2, 2)))
-            modelA.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-            modelA.add(MaxPooling2D(pool_size=(2, 2)))
-            modelA.add(Flatten())
-            modelA.add(Dense(256, activation='relu'))
-            modelA.add(Dense(128, activation='relu'))
-            modelA.add(Dense(100, activation='softmax'))
-            modelA.compile(loss='sparse_categorical_crossentropy', optimizer = 'sgd', metrics=['accuracy'])
-            modelA.fit(X_train, Y_train , epochs=200, batch_size=16, verbose=1)
-            modelA.save('saved_models/cifar100_1')
+            if not(path.exists('saved_models/cifar100_2.h5')) or self.flag:
+                modelA = Sequential()
+                modelA.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+                modelA.add(MaxPooling2D(pool_size=(2, 2)))
+                modelA.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+                modelA.add(MaxPooling2D(pool_size=(2, 2)))
+                modelA.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+                modelA.add(MaxPooling2D(pool_size=(2, 2)))
+                modelA.add(Flatten())
+                modelA.add(Dense(256, activation='relu'))
+                modelA.add(Dense(128, activation='relu'))
+                modelA.add(Dense(100, activation='softmax'))
+                modelA.compile(loss='sparse_categorical_crossentropy', optimizer = 'sgd', metrics=['accuracy'])
+                modelA.fit(X_train, Y_train , epochs=200, batch_size=16, verbose=1)
+                modelA.save('saved_models/cifar100_1.h5')
 
             
             #return model
             #model.save('saved_models/cifar10_3')
             
             #model 2
-            modelB = Sequential()
-            modelB.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-            modelB.add(MaxPooling2D(pool_size=(2, 2)))
-            modelB.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-            modelB.add(MaxPooling2D(pool_size=(2, 2)))
-            modelB.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-            modelB.add(MaxPooling2D(pool_size=(2, 2)))
-            modelB.add(Flatten())
-            modelB.add(Dense(256, activation='relu'))
-            modelB.add(Dense(128, activation='relu'))
-            modelB.add(Dense(100, activation='softmax'))
-            modelB.compile(loss='binary_crossentropy', optimizer = 'sgd', metrics=['accuracy'])
-            modelB.fit(X_train, Y_train , epochs=200, batch_size=16, verbose=1)
-            modelB.save('saved_models/cifar100_2')
+            if not(path.exists("saved_models/cifar10_2.h5")) or self.flag:
+                M_Y_train = self.binary_data(M_Y_train)
+                modelB = Sequential()
+                modelB.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+                modelB.add(MaxPooling2D(pool_size=(2, 2)))
+                modelB.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+                modelB.add(MaxPooling2D(pool_size=(2, 2)))
+                modelB.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+                modelB.add(MaxPooling2D(pool_size=(2, 2)))
+                modelB.add(Flatten())
+                modelB.add(Dense(256, activation='relu'))
+                modelB.add(Dense(128, activation='relu'))
+                modelB.add(Dense(100, activation='softmax'))
+                modelB.compile(loss='binary_crossentropy', optimizer = 'sgd', metrics=['accuracy'])
+                modelB.fit(M_X_train, M_Y_train , epochs=200, batch_size=16, verbose=1)
+                modelB.save('saved_models/cifar100_2.h5')
 
 
 
@@ -115,7 +128,7 @@ class model:
             M_X_train = X_train
             M_Y_train = Y_train
             #model 1
-            if not(path.exists("saved_models/MNIST_1.h5")):
+            if not(path.exists("saved_models/MNIST_1.h5")) or self.flag:
                 modelA = Sequential()
                 modelA.add(Conv2D(32,(3,3), activation='relu', input_shape=(28,28,1)))
                 modelA.add(MaxPooling2D(2,2))
@@ -128,15 +141,12 @@ class model:
                 modelA.compile(loss='sparse_categorical_crossentropy', optimizer = 'sgd', metrics=['accuracy'])
                 modelA.fit(X_train, Y_train , epochs=20, batch_size=32, verbose=1)
                 modelA.save('saved_models/MNIST_1.h5')
-            else:
-                print("else option")
-                new_model = tf.keras.models.load_model('saved_models/MNIST_1.h5')
                 
 
 
             
             #model 2
-            if not(path.exists("saved_models/MNIST_2.h5")):
+            if not(path.exists("saved_models/MNIST_2.h5")) or self.flag:
                 M_Y_train = self.binary_data(M_Y_train)
                 modelB = Sequential()
                 modelB.add(Conv2D(32,(3,3), activation='relu', input_shape=(28,28,1)))
@@ -147,9 +157,6 @@ class model:
                 modelB.compile(loss='binary_crossentropy', optimizer = 'sgd', metrics=['accuracy'])
                 modelB.fit(M_X_train, M_Y_train , epochs=20, batch_size=32, verbose=1)
                 modelB.save('saved_models/MNIST_2.h5')
-            else:
-                print("else option")
-                new_model = tf.keras.models.load_model('saved_models/MNIST_2.h5')
 
 
 
@@ -160,7 +167,7 @@ class model:
         for i in model:
             model = self.CNN(i)
         '''
-        model = self.CNN('MNIST')
+        model = self.CNN('CIFAR10')
 
     def binary_data(self, Y_train):
         array = []
@@ -171,6 +178,20 @@ class model:
             array.append(sam)
         array = np.array(array)
         return array
+
+    def set_flag(self,value):
+        '''set the flag for training to be passed in value as parameter'''
+        if value:
+            self.flag=value
+        elif not(value):
+            self.flag=value
+        else:
+            self.flag=False
+            raise ValueError("Expected boolean value. Setting default to False")
+
+    def flip_flag(self):
+        '''Flips the flag: example False -> True and True -> False''' 
+        self.flag = not(self.flag)
 
 
 if __name__=="__main__":
